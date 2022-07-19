764780725e11c0edd26effc122142bd3f289a4d4
Nuri-Lee <nuri.lee.dev@gmail.com>
2022-07-19 23:23:32 +0900

    객체를 테이블에 맞추어 데이터 중심으로 모델링

--- src/main/java/helloJpa/JpaMain.java
+++ src/main/java/helloJpa/JpaMain.java
@@ -15,41 +15,23 @@ public class JpaMain {
        tx.begin();
 
        try {
-           Member member1 = new Member();
-           member1.setUsername("A");
 
-           Member member2 = new Member();
-           member2.setUsername("B");
+           Team team = new Team();
+           team.setName("TeamA");
+           em.persist(team);    // id 에 값이 들어감 -> 영속상태가 되면 무조건 pk 값이 세팅됨
 
-           Member member3 = new Member();
-           member3.setUsername("C");
+           Member member = new Member();
+           member.setUsername("member1");
+           member.setTeamId(team.getId());
+           em.persist(member);
 
-           System.out.println("========================");
+           Member findMember = em.find(Member.class, member.getId());
+           Long findTeamId = findMember.getTeamId();
+           Team teamId = em.find(Team.class, findTeamId);
 
-           // call next value for MEMBER_SEQ 이 2번 호출되는데,
-           // 처음 호출하면 call next value for MEMBER_SEQ 는 1이 된다.
-           // 그런데 지금 성능 최적화를 하려고 했기 때문에
-           // 두 번째 호출을 하게 되는데 이때는 call next value for MEMBER_SEQ 가 51이다.
-           // -> 50개씩 호출 해야하는데, 처음 호출시 1이어서 문제가 있다 싶어 또 호출한 것이다.
-           // 그 다음 호출되는 값은 51일이지만, 현재 내 호출된 SEQUENCE 값은 2이다.
-           // 즉, DB SEQ | 현재 SEQ
-           //       1       1
-           //       51      2
-           //       51      3
-           // 이렇게 되는 것이다.
-           // 2번의 call next 가 이뤄지지만, 1,2,3,, 씩 증가하다가 51을 만나면 또 50씩 증가해야하기 때문에
-           // 이때 또 call next 가 호출된다.
+           // -> 객체지향스럽지 않은 방식식
 
-           em.persist(member1); // DB 호출 :: 1, 51
-//           em.persist(member2);   // MEMORY 호출
-//           em.persist(member3);   // MEMORY 호출
-
-           System.out.println("member1 = " + member1.getId());
-           System.out.println("member2 = " + member2.getId());
-           System.out.println("member3 = " + member3.getId());
-
-           System.out.println("===========================");
-           tx.commit();
+          tx.commit();
        } catch (Exception e){
            tx.rollback();
        } finally {
--- src/main/java/helloJpa/Member.java
+++ src/main/java/helloJpa/Member.java
@@ -5,28 +5,18 @@ import net.bytebuddy.dynamic.loading.InjectionClassLoader;
 import javax.persistence.*;
 
 @Entity
-@SequenceGenerator(
-        name = "MEMBER_SEQ_GENERATOR",
-        sequenceName = "MEMBER_SEQ", // 매핑할 데이터베이스 시퀀스 이름
-        // allocationSize = 50 :
-        // next call 할 때 미리 50개를 불러오는 것 (최적화 향상)
-        // -> 매번 next call 로 가져오면 네트워크를 타야하기 때문에 성능 문제가 있다.
-        // 이론적으로 allocationSize 값을 증가시키면 좋지만, ( ex 만개)
-        // 하지만 이렇게 했을 때 웹서버가 내려갔을 때 다 날아간다 ..
-        // 그 사이 숫자에 구멍이 생긴다. -> 괜히 낭비가 됨
-        // 즉, 50~100이 적절하다.
-        // 다만 서버가 여러 대면 상관없다.
-        initialValue = 1, allocationSize = 50
-)
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,
-                    generator = "MEMBER_SEQ_GENERATOR")
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "USERNAME")
     private String username;
+
+    @Column(name="TEAM_ID")
+    private Long teamId;
 
     public Long getId() {
         return id;
@@ -44,6 +34,11 @@ public class Member {
         this.username = username;
     }
 
-    public Member() {
+    public Long getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Long teamId) {
+        this.teamId = teamId;
     }
 }
